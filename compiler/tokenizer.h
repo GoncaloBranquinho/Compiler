2bdbca558a0805b5e1954dfcc9e60f6c71b7f0fa
Denellyne <gustavobarbosavs@gmail.com>
2025-03-31 10:21:06 +0100

    Fuzzer

--- compiler/tokenizer.h
+++ compiler/tokenizer.h
@@ -1,31 +1,30 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
+#include <string>
 #include <unordered_set>
-#include <vector>
 #include <utility>
+#include <vector>
 
 enum class Token {
-    KEYWORD,    
-    INCLUDE,
-    ID,
-    NUMBER,
-    OPERATOR,
-    DELIMITER,
-    PUNCTUATION,
-    LESS_THAN,
-    GREATER_THAN,
-    BOOLEAN,
-    ASSIGN,
-    NEGATION,
-    PREPROCESSOR,
-    UNKNOWN
+  KEYWORD,
+  INCLUDE,
+  ID,
+  NUMBER,
+  OPERATOR,
+  DELIMITER,
+  PUNCTUATION,
+  LESS_THAN,
+  GREATER_THAN,
+  BOOLEAN,
+  ASSIGN,
+  NEGATION,
+  PREPROCESSOR,
+  UNKNOWN
 };
 
 std::string toString(Token token);
 
-
-void tokenize(std::string& s, std::vector<std::pair<std::string,Token>>& tokens);
+void tokenize(std::string &s,
+              std::vector<std::pair<std::string, Token>> &tokens);
 
 #endif
-
-
